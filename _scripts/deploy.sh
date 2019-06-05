13d5f1f2dfb71abf19cad38d2b6e1d3e92ba9555
sgroth <stefangroth@me.com>
2019-06-05 18:36:30 +0200

    Update deploy.sh

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
 sshpass -p "$PASSWORD" ssh "$USERNAME"@"$HOST" rm -rf html/stefangroth.de
-
 sshpass -p "$PASSWORD" ssh "$USERNAME"@"$HOST" mkdir html/stefangroth.de
-
-sshpass -p "$PASSWORD" scp -v -o User="$USERNAME" -o Port=22 -r _site/* "$HOST":/var/www/web23683373/html/stefangroth.de
+# sshpass -p "$PASSWORD" scp -v -o User="$USERNAME" -o Port=22 -r _site/* "$HOST":/var/www/web23683373/html/stefangroth.de
